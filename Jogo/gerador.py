e5391bbde7fad6857246aeefddf1e72ae821ada7
Felipe Lambak <Lambakfelipe@gmail.com>
2024-07-23 17:03:49 -0300

    ajustes gerador

--- Jogo/gerador.py
+++ Jogo/gerador.py
@@ -2,10 +2,11 @@ import random
 
 def gera_numeros():
     saida= []
-    saida.append(random.randint(1,99))
-    while len(saida)<3:
+    saida.append(random.randint(1,99)) #Seleciona 1° Num
+
+    while len(saida)<3: #Adiciona mais 2 números diferentes
         proxNum = random.randint(1,99)
-        if proxNum != saida[-1]:
+        if proxNum not in saida:
             saida.append(proxNum)
     errado = random.randint(0,2)
     soma=0
@@ -14,5 +15,4 @@ def gera_numeros():
             soma += n
     saida.append(soma)
 
-    return saida
-print(gera_numeros())
+    return (saida,errado) #Retorna a lista de numeros e a posição do errado
