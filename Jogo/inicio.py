17a51f402a3d4be14d6c1f12431d8c430f26f2c8
Felipe Lambak <Lambakfelipe@gmail.com>
2024-07-23 15:23:32 -0300

    TESTE TELA INICIAL

--- Jogo/inicio.py
+++ Jogo/inicio.py
@@ -1,2 +1,37 @@
 import pygame
-from config import GAME, FPS, QUIT,
+from config import GAME, FPS, QUIT, WIDTH, HEIGHT
+
+def tela_incio(tela):
+    clock = pygame.time.Clock()
+    rodando = True
+    fonte = pygame.font.SysFont(None,40)
+    textoPlay = fonte.render("Jogar ",True,(0,255,0))
+    playwidth = textoPlay.get_width()
+    playheight = textoPlay.get_height()
+    buttonwidth = playwidth + 100
+    buttonheight = playheight + 50
+    rectbutton = pygame.Rect((WIDTH/2 - buttonwidth/2 , HEIGHT/2 - buttonheight/2, buttonwidth, buttonheight))
+
+    
+    while rodando:
+        clock.tick(FPS)
+
+        mousePos = pygame.mouse.get_pos()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                state = QUIT
+                rodando = False
+            if pygame.Rect.collidepoint(rectbutton,mousePos) and pygame.mouse.get_pressed()[0]:
+                state= GAME
+                rodando = False
+            
+
+
+        tela.fill((255,0,255))
+        pygame.draw.rect(tela,(0,0,0),rectbutton)
+        tela.blit(textoPlay,(WIDTH/2 - playwidth/2, HEIGHT/2 - playheight/2))
+        
+        
+        pygame.display.flip()
+
+    return state
--- Jogo/jogo.py
+++ Jogo/jogo.py
@@ -2,6 +2,7 @@
 import pygame
 import random
 from config import WIDTH, HEIGHT, FPS, INICIO, GAME, GAMEOVER, QUIT
+from inicio import tela_incio
 
 pygame.init()
 
@@ -11,5 +12,7 @@ state= INICIO
 while state != QUIT:
     if state == INICIO:
         #Rodar a tela inicial------
-        
+        state = tela_incio(tela)
+    elif state == GAME:
+        state ==QUIT
     
