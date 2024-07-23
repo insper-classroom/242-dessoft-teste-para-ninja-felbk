ab0c43d486253c57020ed5a6e8c015460f123de3
Felipe Lambak <Lambakfelipe@gmail.com>
2024-07-23 17:38:25 -0300

    Inicio blocos

--- Jogo/fase.py
+++ Jogo/fase.py
@@ -6,14 +6,20 @@ def tela_fase(tela):
     clock= pygame.time.Clock()
     rodando = True
 
+
     while rodando:
         clock.tick(FPS)
 
         #Configura o Chão
-        chãoHeight = 30 #Altura
+        chãoHeight = 40 #Altura
         # Cria retangulo  pygame.Rect( Esquerda, Topo , Largura, Altura)
         chãoRect = pygame.Rect( 0,  HEIGHT-chãoHeight,  WIDTH ,  chãoHeight) 
-        chãoCollor = (233,150,233)
+        chãoCollor = (233,150,233) # Código RGB da cor
+
+        #Cria grupo de blocos
+        blocos = pygame.sprite.Group()
+
+        
 
         #Analisa eventos
         for event in pygame.event.get():
@@ -25,7 +31,7 @@ def tela_fase(tela):
 
         #Preenche a tela
         tela.fill((255,255,255))
-        pygame.draw.rect(tela,chãoCollor, chãoRect)
+        pygame.draw.rect(tela,chãoCollor, chãoRect) #Desenha chão
         
         #Atualiza tela
         pygame.display.flip()
