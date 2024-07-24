564e9418be17d4c504f9174d9e225b4d43256139
Felipe Lambak <Lambakfelipe@gmail.com>
2024-07-24 14:24:42 -0300

    Blocos aparecendo e soma também

--- Jogo/fase.py
+++ Jogo/fase.py
@@ -1,5 +1,4 @@
 import pygame
-from pygame.sprite import _Group
 from config import GAME, QUIT , INICIO, WIDTH,HEIGHT, GAMEOVER, FPS
 from gerador import gera_numeros
 
@@ -8,6 +7,11 @@ def tela_fase(tela):
     rodando = True
     Timer = 60 # 60 segundos
     font = pygame.font.SysFont(None,35)
+
+
+       
+    #Cria grupo de blocos
+    blocos = pygame.sprite.Group()
 
     #Classe do bloco
     class Bloco(pygame.sprite.Sprite):
@@ -20,22 +24,29 @@ def tela_fase(tela):
             self.errado = errado
             #Adiciona o bloco criado ao grupo de blocos para facilitar o controle
             self.add(blocos)
-            
+
             self.size = (120,120)
             self.pos = pos
             self.rect = (self.pos[0],self.pos[1],self.size[0],self.size[1])
             
             #Configura a mensagem a ser exibida ao clicar no bloco
-            if errado:
+            if self.errado:
                 self.mensagem = "Errado!"
             else:
                 self.mensagem = "Correto!"
 
 
-        #Atualiza e analisa eventos com blocos 
-        def update(self):
+        #Atualiza exibição de blocos 
+        def update(self,tela):
              
-            if pygame.Rect.collidepoint(self.rect,pygame.mouse.get_pos()):
+            #Exibe o bloco e o valor
+            pygame.draw.rect(tela,self.cor,self.rect)
+            valor = font.render(str(self.valor),True,(0,0,0))
+            valorWidth= valor.get_width()
+            valorHeight= valor.get_height()
+            tela.blit(valor,(self.pos[0]+ self.size[0]/2 - valorWidth/2 , self.pos[1] + self.size[1]/2 - valorHeight/2))
+            return
+
 
             
 
@@ -61,8 +72,7 @@ def tela_fase(tela):
         chãoRect = pygame.Rect( 0,  HEIGHT-chãoHeight,  WIDTH ,  chãoHeight) 
         chãoCollor = (233,150,233) # Código RGB da cor
 
-        #Cria grupo de blocos
-        blocos = pygame.sprite.Group()
+     
             
         #Configura blocos
         CoresBlocos = [(255,0,0),(0,255,0),(0,0,255)]
@@ -72,6 +82,16 @@ def tela_fase(tela):
             Num_e_resposta = gera_numeros() #Retorna uma tupla ( [n1,n2,n3,soma] , N° da pos errada )
             num = Num_e_resposta[0] 
             PosErrada = Num_e_resposta[1]
+            for i in range(3):
+                if PosErrada == i:
+                    errado = True
+                else:
+                    errado= False
+                bloco = Bloco(num[i],CoresBlocos[i], (WIDTH/2 - 60 ,HEIGHT - chãoHeight - (120*(i+1))), errado )
+        
+        sm = font.render(str(num[3]),True,(0,0,0))
+
+                
         
         
 
@@ -97,6 +117,10 @@ def tela_fase(tela):
         tela.fill((255,255,255))
         pygame.draw.rect(tela,chãoCollor, chãoRect) #Desenha chão
         tela.blit(Tempo,(WIDTH - TempoWidth - 20, 0 ))
+        tela.blit(sm,(5,5))
+
+        #desenha todos blocos
+        blocos.update(tela)
         
         #Atualiza tela
         pygame.display.flip()
